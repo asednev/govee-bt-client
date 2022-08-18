fcc10b1d746d7e4b4a8372cef430c92600cb4438
Alexey <3236025+asednev@users.noreply.github.com>
2022-08-18 15:38:12 -0700

    1.0.15

--- dist/validation.js
+++ dist/validation.js
@@ -7,7 +7,7 @@ const h5101_uuid_rev = "0100";
 const h5179_uuid_rev = "0188";
 exports.isHt5074 = (hex) => hex.includes(h5074_uuid_rev) && hex.length == 18; // Govee H5074
 exports.isHt5075 = (hex) => hex.includes(h5075_uuid_rev) && hex.length == 16; // Govee H5072/H5075
-exports.isHt5101 = (hex) => hex.includes(h5101_uuid_rev); // Govee H5101/H5102
+exports.isHt5101 = (hex) => hex.includes(h5101_uuid_rev); // Govee H5100/H5101/H5102
 exports.isHt5179 = (hex) => hex.includes(h5179_uuid_rev) && hex.length == 22; // Govee H5179
 exports.isValidPeripheral = (peripheral) => {
     const { address, advertisement } = peripheral;
